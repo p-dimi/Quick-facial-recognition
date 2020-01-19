806c40c48f75e277c94233894b7f42930d8b784f
p-dimi <41579855+p-dimi@users.noreply.github.com>
2020-01-19 20:34:11 +0100

    Add files via upload

--- face_recog.py
+++ face_recog.py
@@ -10,9 +10,10 @@ import numpy as np
 import os
 
 class face_recognizer:
-    def __init__(self, recognition_threshold = 0.5, size_modifier = 0.75, known_faces = 'known_faces/', predictor_path = 'dlib_models/shape_predictor_68_face_landmarks.dat', recognition_model_path = 'dlib_models/dlib_face_recognition_resnet_model_v1.dat', display_window = True):
+    def __init__(self, recognition_threshold = 0.5, size_modifier = 0.75, init_size_modifier = 0.75, known_faces = 'known_faces/', predictor_path = 'dlib_models/shape_predictor_68_face_landmarks.dat', recognition_model_path = 'dlib_models/dlib_face_recognition_resnet_model_v1.dat', display_window = False):
         # size modifier for faster processing
         self.size_modifier = size_modifier
+        self.init_size_modifier = init_size_modifier
         # recognition threshold - how similar must the faces be to be considered the same person
         self.threshold = recognition_threshold
         
@@ -33,27 +34,36 @@ class face_recognizer:
             self.window_rec = dlib.image_window()
             
     def invert_channels(self, img):
+        # self explanatory - inverts BGR to RGB
         img_n = img.copy()
         img_n[:,:,0] = img[:,:,2]
         img_n[:,:,2] = img[:,:,0]
         return img_n
     
-    def process_faces(self, img):
+    def process_faces(self, img, proc_type = 'multiple'):
+        # processes the faces in known_faces folder, turning them into 128D vectors, in order to speed up runtime
+        # this prevents having to turn the (known) faces into vectors every single time during runtime
         detected = self.detector(img, 1)
-        d = detected[0]
-        shape = self.shape_predictor(img, d)
-        return d, shape
+        if proc_type == 'single':
+            d = detected[0]
+            shape = self.shape_predictor(img, d)
+            return d, shape
+        elif proc_type == 'multiple':
+            shapes = []
+            for d in detected:
+                shapes.append(self.shape_predictor(img, d))
+            return detected, shapes
     
     def initial_vector_processing(self):
         # initialize recognizer for faster real time performance
         for i in range(len(self.known_faces)):
             pth = ''.join([self.known_faces_dir, self.known_face_files[i]])
-            compared_face_img = cv2.resize(cv2.imread(pth), (0,0), fx=self.size_modifier, fy=self.size_modifier)
+            compared_face_img = cv2.resize(cv2.imread(pth), (0,0), fx=self.init_size_modifier, fy=self.init_size_modifier)
             compared_face_img = self.invert_channels(compared_face_img)
             
             person_name = self.known_faces[i]
             
-            compared_d, compared_shape = self.process_faces(compared_face_img)
+            compared_d, compared_shape = self.process_faces(compared_face_img, proc_type = 'single')
             
             compared_face_vector = self.face_recognizer.compute_face_descriptor(compared_face_img, compared_shape)
             
@@ -79,33 +89,29 @@ class face_recognizer:
         
         # process faces
         try:
-            d, shape = self.process_faces(img)
-            # get 128D point vector
-            face_vector = self.face_recognizer.compute_face_descriptor(img, shape)
-            # process known faces, and compare similarities
-            known_image_path, name, similarity = self.compare_with_knowns(face_vector)
+            ds, shapes = self.process_faces(img, proc_type = 'multiple')
+            recognized_faces = {}
             
-            if known_image_path == None and name == None and similarity == None:
-                return None, None
             
-            if self.display_window:
-                # clear out window to display new frame
-                self.window.clear_overlay()
-                self.window.set_image(img)
+            for i in range(len(shapes)):
+                d = ds[i]
+                shape = shapes[i]
+                # get 128D point vector
+                face_vector = self.face_recognizer.compute_face_descriptor(img, shape)
+                # process known faces, and compare similarities
+                known_image_path, name, similarity = self.compare_with_knowns(face_vector)
+
+                # divided by size_modifier to resize to original picture size
+                face_box_pos = [(int(d.left()/self.size_modifier), int(d.top()/self.size_modifier)), (int(d.right()/self.size_modifier), int(d.bottom()/self.size_modifier))]
+
+                if name not in recognized_faces:
+                    recognized_faces[name] = [face_box_pos, similarity]
+                else:
+                    # the person is already in the dict - add a second version of them (in case of showing two+ pictures of the same person in the frame, for example)
+                    recognized_faces[name+str(i)] = [face_box_pos, similarity]
                 
-                #pth = ''.join([self.known_faces_dir, self.known_face_files[index]])
-                compared_face_img = self.invert_channels(cv2.imread(known_image_path))
-                
-                self.window_rec.clear_overlay()
-                self.window_rec.set_image(compared_face_img)
-                
-                # draw face/s
-                self.window.clear_overlay()
-                self.window.add_overlay(d)
-                self.window.add_overlay(shape)
-            
-            # return the name of the person and percentage of similarity
-            return name, similarity
+            # return dict containing the name of the person and percentage of similarity for each recognized face
+            return recognized_faces
         except:
             print('No face detected in frame')
-            return None, None
+            return None
